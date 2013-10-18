1a878bcfbc646ea36e5353703dd6926d856fcb86
Chiel Kunkels <chiel@kunkels.me>
2013-10-18 11:49:13 +0200

    Basic in-memory caching

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * New relic
+ */
+var Relic = function(){
+	this.cache = {};
+};
+
+/**
+ * Set a new key into the cache
+ * @param {String} key
+ * @param {Mixed} value
+ * @param {Number} ttl
+ */
+Relic.prototype.set = function(key, value, ttl){
+	var record = this.cache[key] || {};
+
+	if (record.timeout){
+		clearTimeout(record.timeout);
+	}
+
+	record.expires = ttl + (+new Date());
+	record.value = value;
+	record.timeout = setTimeout(function(){
+		this.del(key);
+	}.bind(this), ttl);
+
+	this.cache[key] = record;
+};
+
+/**
+ * Get a key from the cache
+ * @param {String} key
+ */
+Relic.prototype.get = function(key){
+	var record = this.cache[key];
+	return record ? record.value : undefined;
+};
+
+/**
+ * Delete a key from the cache
+ * @param {String} key
+ */
+Relic.prototype.del = function(key){
+	delete this.cache[key];
+};
+
+module.exports = Relic;
+
